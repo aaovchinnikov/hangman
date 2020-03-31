e183a13d740a965884b34841b126a4b16d605ed1
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-03-31 20:18:16 +0300

    Added masked printing to Secret class

--- src/main/java/hangman/Secret.java
+++ src/main/java/hangman/Secret.java
@@ -1,5 +1,7 @@
 package hangman;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Random;
 
 /**
@@ -11,20 +13,41 @@ import java.util.Random;
 public final class Secret {
 	/** The word in question */
 	private final String secret;
+	/** The {@link PrintStream} where Secret instance shoul be printed */
+	private final PrintStream out;
+	/** 
+	 * The array of bits for printing. True bit allows to print real letter,
+	 * false bit indicates that question sign ('?') should be printed instead of letter */  
+	private final boolean[] visible;
 	
 	/**
 	 * Secondary constructor 
 	 * @param variants - array of strings to choose randomly one as a secret
 	 */
-	public Secret(final String[] variants) {
-		this(variants[new Random().nextInt(variants.length)]);
+	public Secret(final String[] variants, final PrintStream out) {
+		this(variants[new Random().nextInt(variants.length)], out);
 	}
 	
-	/** Main constructor
+	/** 
+	 * Secondary constructor. Builds Secret with all letters masked
 	 * @param str - {@link String} encapsulated as secret
 	 */
-	public Secret(final String str) {
+	public Secret(final String str, final PrintStream out) {
+		this(str, new boolean[str.length()], out);
+	}
+	
+	/** 
+	 * Main constructor. Builds Secret with letters masked using {@link m} array
+	 * @param str - {@link String} encapsulated as secret
+	 * @param m - mask of visible positions, visible position should be marked with true bits 
+	 */
+	public Secret(final String str, final boolean[] m, final PrintStream out) {
+		if (str.length() != m.length) {
+			throw new IllegalArgumentException("Provided string length and mask length don't match");
+		}
 		this.secret = str;
+		this.visible = m;
+		this.out = out;
 	}
 	
 	/**
@@ -35,4 +58,35 @@ public final class Secret {
 	public boolean contains(final char c) {
 		return this.secret.indexOf(c) >=0 ;
 	}
+	
+	/**
+	 * 
+	 */
+	public void print() {
+		for (int i = 0; i < this.secret.length(); ++i) {
+			if (this.visible[i]) {
+                this.out.print(this.secret.charAt(i));
+            } else {
+                this.out.print("?");
+            }
+		}
+		this.out.print("\n\n");
+	}
+	
+	/**
+	 * 
+	 */
+/*	public Secret unmask(final char c) {
+		boolean[] m = new boolean[this.secret.length()];
+        for (int i = 0; i < word.length(); ++i) {
+            if (word.charAt(i) == chr && !visible[i]) {
+                visible[i] = true;
+                hit = true;
+            }
+        }
+
+		return new Secret(this.secret,)
+	}
+*/
+
 }
--- src/test/java/hangman/SecretTest.java
+++ src/test/java/hangman/SecretTest.java
@@ -1,6 +1,10 @@
 package hangman;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.Test;
 
@@ -8,11 +12,21 @@ public class SecretTest {
 
 	@Test
 	public void containTest() {
-		assertTrue(new Secret("Test").contains('t'));
+		assertTrue(new Secret("Test", System.out).contains('t'));
 	}
 
 	@Test
 	public void notContainTest() {
-		assertFalse(new Secret("Test").contains('c'));
+		assertFalse(new Secret("Test", System.out).contains('c'));
+	}
+	
+	@Test
+	public void printMasked() {
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		final PrintStream out = new PrintStream(baos);
+		Secret secret = new Secret("Test", out);
+		secret.print();
+		String output = baos.toString();
+		assertTrue(output.equals("????\n\n"));
 	}
 }
