5a193316cf597e860af34d3e296238489bcf422d
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-03-31 23:52:53 +0300

    Switched to encapsulate PrintStream instead of OutputStream

--- src/main/java/hangman/Hangman.java
+++ src/main/java/hangman/Hangman.java
@@ -1,6 +1,5 @@
 package hangman;
 
-import java.io.OutputStream;
 import java.io.PrintStream;
 
 /**
@@ -12,13 +11,13 @@ public final class Hangman implements Game {
 	private final Secret secret;
 	private final int max;
 	private final Guess guess;
-	private final OutputStream output;
+	private final PrintStream output;
 
 	/**
 	 * @param secret
 	 * @param max
 	 */
-	public Hangman(Secret secret, int max, Guess guess, OutputStream out) {
+	public Hangman(Secret secret, int max, Guess guess, PrintStream out) {
 		this.secret = secret;
 		this.max = max;
 		this.guess = guess;
@@ -27,24 +26,23 @@ public final class Hangman implements Game {
 
 	public boolean won() {
 		int mistakes = 0;
-		final PrintStream out = new PrintStream(this.output);
 		char chr;
 		Secret sec = this.secret;
 		while (mistakes < this.max) {
 			chr = guess.next();
 			if (this.secret.contains(chr)) {
-				out.print("Hit!\n");
+				this.output.print("Hit!\n");
 				sec = sec.unmask(chr);
             } else {
               	mistakes++;
-              	out.printf(
+              	this.output.printf(
               		"Missed, mistake #%d out of %d\n",
                     mistakes, this.max
               	);
             }
 			sec.print();
-			if (this.secret.unmasked()) {
-				return true;
+			if (sec.unmasked()) {
+				return true; 
 			}
 		}
 		return false;
