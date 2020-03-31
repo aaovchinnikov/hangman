1e943394ce84de9c981d12d7318a2d7a86829d7f
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-03-31 19:35:34 +0300

    Remove indirect dependancy of Hangman on Scanner class
    Scanner is used directly in Guess. Switched to use encapsulated Guess
    instance

--- src/main/java/hangman/Hangman.java
+++ src/main/java/hangman/Hangman.java
@@ -1,10 +1,7 @@
 package hangman;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
 
 /**
  * 
@@ -14,39 +11,34 @@ import java.util.Scanner;
 public final class Hangman implements Game {
 	private final Secret secret;
 	private final int max;
-	private final InputStream input;
+	private final Guess guess;
 	private final OutputStream output;
 
 	/**
 	 * @param secret
 	 * @param max
 	 */
-	public Hangman(Secret secret, int max, InputStream in, OutputStream out) {
+	public Hangman(Secret secret, int max, Guess guess, OutputStream out) {
 		this.secret = secret;
 		this.max = max;
-		this.input = in;
+		this.guess = guess;
 		this.output = out;
 	}
 
 	public boolean won() {
 		int mistakes = 0;
-		try (
-			final PrintStream out = new PrintStream(this.output);
-			final Scanner scanner = new Scanner(this.input)
-		){
-			Guess guess = new Guess(scanner, this.output);
-			while (mistakes < this.max) {
-				if (this.secret.contains(guess.next())) {
-	                	out.print("Hit!\n");
-	                } else {
-	                	mistakes++;
-	                    out.printf(
-	                    		"Missed, mistake #%d out of %d\n",
-	                    		mistakes, this.max
-	                    );
-	                }
-				}
-			return false;
-		}	
+		final PrintStream out = new PrintStream(this.output);
+		while (mistakes < this.max) {
+			if (this.secret.contains(guess.next())) {
+                	out.print("Hit!\n");
+                } else {
+                	mistakes++;
+                    out.printf(
+                    		"Missed, mistake #%d out of %d\n",
+                    		mistakes, this.max
+                    );
+                }
+			}
+		return false;
 	}
 }
--- src/main/java/hangman/OopMain.java
+++ src/main/java/hangman/OopMain.java
@@ -1,17 +1,21 @@
 package hangman;
 
 import java.io.OutputStream;
+import java.util.Scanner;
 
 public class OopMain {
 
 	public static void main(String[] args) {
-		OutputStream out = System.out;
+		final OutputStream out = System.out;
 		new Result(
 			out, 
 			new Hangman(
 				new Secret("secret"),
 				5,
-				System.in,
+				new Guess(
+					new Scanner(System.in),
+					out
+				),
 				out
 			)
 		).print();
