e20c854aea5591c4204fa938d397af3d19fd7716
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-03-31 18:43:13 +0300

    Fixed issues related to buffered input reading
    Moved out Scanner instance from class and changed it with encapsulated
    Iterator<String>

--- src/main/java/hangman/Guess.java
+++ src/main/java/hangman/Guess.java
@@ -1,27 +1,25 @@
 package hangman;
 
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
+import java.util.Iterator;
 
 public class Guess {
-	private final InputStream input;
+	private final Iterator<String> iterator;
 	private final OutputStream output;
 
 	/**
 	 * @param input
 	 * @param output
 	 */
-	public Guess(InputStream input, OutputStream output) {
-		this.input = input;
+	public Guess(Iterator<String> iterator, OutputStream output) {
+		this.iterator = iterator;
 		this.output = output;
 	}
 
 	public char next() {
 		final PrintStream out = new PrintStream(this.output);
-		final Scanner scanner = new Scanner(this.input);
 		out.print("Guess a letter: ");
-		return scanner.next().charAt(0);
+		return iterator.next().charAt(0);
 	}
 }
--- src/test/java/hangman/GuessTest.java
+++ src/test/java/hangman/GuessTest.java
@@ -3,20 +3,34 @@ package hangman;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
-public class GuessTest {
+public final class GuessTest {
 
 	@Test
 	public void guessTest() {
 		char five = '5';
 		final String str = new StringBuilder().append(five).append('\n').toString();
 		Guess guess = new Guess(
-			new ByteArrayInputStream(str.getBytes()),
+			new Scanner(new ByteArrayInputStream(str.getBytes())),
 			System.out
 		);
 		assertThat(guess.next(), CoreMatchers.equalTo(five));
+	}
+
+	@Test
+	public void twoGuessesTest() {
+		final char five = '5';
+		final char six = '6';
+		final String str = new StringBuilder().append(five).append('\n').append(six).append('\n').toString();
+		final Guess guess = new Guess(
+			new Scanner(new ByteArrayInputStream(str.getBytes())),
+			System.out
+		);
+		assertThat(guess.next(), CoreMatchers.equalTo(five));
+		assertThat(guess.next(), CoreMatchers.equalTo(six));
 	}
 }
